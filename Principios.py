c9616309701a7854b57e5d466f41810040deae0c
RayanSt <49204068+RayanSt@users.noreply.github.com>
2019-09-23 23:58:14 -0500

    3 principios

--- Principios.py
+++ Principios.py
@@ -1,17 +1,21 @@
-class Coche():
-    __marca = ""
-
-    def __init__(self,marca):
-        self.__marca = marca
-
-    def getMarca(self):
-        return  self.__marca
+import abc
+from abc import ABCMeta
 
 
+#class Coche():
+#    __marca = ""
+#
+#    def __init__(self,marca):
+#        self.__marca = marca
+#
+#    def getMarca(self):
+#        return  self.__marca
 
 
 
-class CocheBD():
+
+
+class CocheBD(object):
     nombre = ""
     def GuardarDato(self, nombre):
         self.nombre = nombre
@@ -20,4 +24,37 @@ class CocheBD():
 
     def EliminarDato(self):
         self.nombre = ""
+
+
+
+class Coche(object):
+    __metaclass__ = ABCMeta
+    @abc.abstractmethod
+    def precioMedioCoche(self):
+        pass
+
+
+class Renault(Coche):
+    def precioMedioCoche(self):
+        return 18000
+
+
+class Audi (Coche):
+    def precioMedioCoche(self):
+        return 25000
+
+
+class Mercedes(Coche):
+    def precioMedioCoche(self):
+        return 27000
+
+def imprimirPrecioMedioCoche(arrayCoches):
+        print(str(arrayCoches.precioMedioCoche()))
+
+arrayCoches = [ Renault(), Audi(), Mercedes() ]
+print(Renault.precioMedioCoche())
+#imprimirPrecioMedioCoche(arrayCoches);
+
+
+
 
